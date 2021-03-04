8ef3638838b40f17340e765163b95a1323b6d0df
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-04 16:24:12 -0500

    Refactor API service objects so multiple API plugins could be utilized with get_info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ before_action :repo_object,
               :pull_objects
 
   def repo_object
-    @repo ||= ApiService.repo_object
+    @repo ||= GithubService.repo_object
   end
 
   def contributor_objects
-    @contributors ||= ApiService.contributor_objects
+    @contributors ||= GithubService.contributor_objects
   end
 
   def pull_objects
-    @pulls ||= ApiService.pull_objects
+    @pulls ||= GithubService.pull_objects
   end
 end
---
+++ app/services/github_service.rb
@@ -0,0 +1,55 @@
+class GithubService < ApiService
+  def self.repo
+    get_info('')[:name]
+  end
+
+  def self.repo_object
+    if repo.nil?
+      'API Limit Exceeded: Repository Info Not Available'
+    else
+      Repo.new(repo).name
+    end
+  end
+
+  def self.contributors
+    get_info('/contributors')
+  end
+
+  def self.contributor_objects
+    if contributors.is_a?(Hash)
+      'API Limit Exceeded: Contributor Info Not Available'
+    else
+      collaborators = [contributors[0], contributors[1], contributors[3], contributors[4]]
+      people = collaborators.map do |data|
+        Contributor.new(data)
+      end
+      people.map do |people|
+        "#{people.user_name} - #{commit_objects(people.user_name)} Commits\n"
+      end.to_sentence
+    end
+  end
+
+  def self.pulls
+    get_info('/pulls?state=all')
+  end
+
+  def self.pull_objects
+    if pulls.is_a?(Hash)
+      'API Limit Exceeded: Pull Request Info Not Available'
+    else
+      pulls.map do |data|
+        Pull.new(data)
+      end.size
+    end
+  end
+
+  def self.commits(user_name)
+    get_info("/commits?author=#{user_name}")
+  end
+
+  def self.commit_objects(user_name)
+    commits(user_name).map do |data|
+      Commit.new(data)
+    end.size
+  end
+end
