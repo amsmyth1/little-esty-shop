39dbe2c69c680058e73f3b4e738204f0596fa81a
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-04 21:09:46 -0500

    Revise view so api sits horizontally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,4 @@ before_action :repo_object
   def repo_object
     @repo = GithubService.repo_object("https://api.github.com/repos/amsmyth1/little-esty-shop")
   end
-
-  # def contributor_objects
-  #   # @contributors ||= GithubService.contributor_objects
-  #   binding.pry
-  #   @contributors ||= @repo.contributors
-  # end
-  #
-  # def pull_objects
-  #   @repo = GithubService.repo_object("https://api.github.com/repos/amsmyth1/little-esty-shop")
-  #
-  #   @pulls ||= @repo.pull_count
-  # end
 end
