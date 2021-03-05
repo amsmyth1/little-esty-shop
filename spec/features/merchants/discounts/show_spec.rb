c486199a8a49dc329f0f63de9793d5710eb142d8
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-05 13:23:53 -0500

    Complete merchant discount show page and passing tests

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "As a merchant" do
   before :each do
-    skip
     @merchant = create(:merchant)
     @discount = create(:discount, merchant_id: @merchant.id)
   end
   describe "when viewing the merchant discount show page" do
     it "should display the discount's quantity threshold and percetage discount" do
-      visit merchant_discount_path(@discount)
+      visit merchant_discount_path(@merchant, @discount)
 
 
       expect(page).to have_content(@discount.name)
       expect(page).to have_content(@discount.threshold)
-      expect(page).to have_content(@discount.percetage)
+      expect(page).to have_content(@discount.percentage)
     end
   end
 end
