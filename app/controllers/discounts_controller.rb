b36cda2d1de2ef82640933f18a0d5c55139b22ac
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-05 14:41:49 -0500

    Complete application code and passing tests for merchant discounts CRUD functionality

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -17,6 +17,25 @@ class DiscountsController < ApplicationController
     redirect_to merchant_discounts_path(merchant)
   end
 
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.update(discount_params)[0]
+
+    if @discount.save
+      flash[:success] = "Your discount has been created!"
+      redirect_to merchant_discount_path(@merchant, @discount)
+    else
+      flash[:error] = "Name cannot be blank. Threshold must be greater than 1.
+      Percentage must be between 0 and 1."
+      render :edit
+    end
+  end
+
   def new
     @merchant = Merchant.find(params[:merchant_id])
     @discount = @merchant.discounts.new
@@ -24,9 +43,6 @@ class DiscountsController < ApplicationController
 
   def create
     @merchant = Merchant.find(params[:merchant_id])
-    # new_merchant_id = (Discount.maximum(:id) + 1)
-
-    # @discount = @merchant.discounts.new(discount_params.merge({id: new_merchant_id}))
     @discount = @merchant.discounts.new(discount_params)
     if @discount.save
       flash[:success] = "Your discount has been created!"
