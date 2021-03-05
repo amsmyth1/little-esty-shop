05a0ce647348d8180f6afb37b5a5068730486823
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-05 12:14:30 -0500

    Add delete discount functionality, add link to create new discount page

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -8,4 +8,15 @@ class DiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
   end
+
+  def destroy
+    merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.find(params[:id])
+    discount.destroy
+
+    redirect_to merchant_discounts_path(merchant)
+  end
+
+  def new
+  end 
 end
