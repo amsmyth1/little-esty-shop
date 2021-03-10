d38b0d8bf30a9210340c87a9f81d92b28fb3b58c
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-03-09 19:34:59 -0500

    Refactor code

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -10,14 +10,6 @@ class DiscountsController < ApplicationController
     @discount = Discount.find(params[:id])
   end
 
-  def destroy
-    merchant = Merchant.find(params[:merchant_id])
-    discount = Discount.find(params[:id])
-    discount.destroy
-
-    redirect_to merchant_discounts_path(merchant)
-  end
-
   def edit
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
@@ -60,6 +52,14 @@ class DiscountsController < ApplicationController
     end
   end
 
+  def destroy
+    merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.find(params[:id])
+    discount.destroy
+
+    redirect_to merchant_discounts_path(merchant)
+  end
+  
   private
   def discount_params
     params.permit(:name, :threshold, :percentage)
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -35,15 +35,6 @@ class InvoiceItem < ApplicationRecord
                       .order(percentage: :desc)
                       .limit(1)
                       .pluck(:id).first
-    # find_by_sql("
-    #   SELECT *
-    #   FROM discounts
-    #   INNER JOIN items ON items.merchant_id = discounts.merchant_id
-    #   INNER JOIN invoice_items ON invoice_items.item_id = items.id
-    #   WHERE #{quantity} >= discounts.threshold AND invoice_items.id = #{self.id}
-    #   ORDER BY discounts.percentage desc
-    #   limit 1
-    #   ")
     if discount_id == [] or discount_id == nil
       []
     else
